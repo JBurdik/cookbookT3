aca676f9f0f73e2364fb6fceaafeb27cc18fd562
Jirka Burdych <burdik6@gmail.com>
2023-04-03 23:31:14 +0200

    recipecard

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 // dayjs plugins
 import cs from "dayjs/locale/cs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { FaClock, FaStar, FaUser } from "react-icons/fa";
 
 const RecipeCard = ({ recipe }: { recipe: Recepty }) => {
   dayjs.extend(relativeTime);
@@ -25,7 +26,7 @@ const RecipeCard = ({ recipe }: { recipe: Recepty }) => {
   return (
     <Link href={`/recipe/${recipe.id}`} className="w-full" key={recipe.id}>
       <div className="group relative flex justify-start gap-4 overflow-hidden rounded-lg  p-4 transition-all nm-flat-bgCol-xs md:h-80 md:grid-cols-2">
-        <div className="relative h-48 w-48 overflow-hidden rounded-xl shadow-md shadow-black">
+        <div className="relative h-48 w-48 flex-shrink-0 overflow-hidden rounded-xl shadow-md shadow-black">
           <Image
             src={recipe.imgUrl}
             alt={recipe.title}
@@ -33,15 +34,24 @@ const RecipeCard = ({ recipe }: { recipe: Recepty }) => {
             className="object-cover"
           />
         </div>
-        <div className="flex grow flex-col items-start">
+        <div className="flex h-full grow flex-col">
           <h2>{recipe.title}</h2>
           <span className=" text-xs">
             {dayjs(recipe.createdAt).format("D. MMMM. YY")}
           </span>
-          <div className="grid grid-cols-3 place-items-center items-center justify-center gap-2">
-            <p>{recipe.time}</p>
-            <p>{recipe.difficulty}</p>
-            <p>{recipe.portions}</p>
+          <div className="mt-8 grid w-full grid-cols-3 place-items-center items-center justify-center gap-2 self-end">
+            <span className="flex flex-col items-center">
+              <FaClock />
+              <p>{recipe.time}</p>
+            </span>
+            <span className="flex flex-col items-center">
+              <FaStar />
+              <p>{Dificulty(recipe.difficulty)}</p>
+            </span>
+            <span className="flex flex-col items-center">
+              <FaUser />
+              <p>{recipe.portions}</p>
+            </span>
           </div>
         </div>
       </div>
